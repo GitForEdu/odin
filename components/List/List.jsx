81da22e1d1a6f397cd53b4f1ce1d14e45204c1de
Tore Stensaker Tefre <tore.s.tefre@ntnu.no>
2021-02-03 14:30:22 +0100

    Custom styles for listtexts

--- components/List/List.jsx
+++ components/List/List.jsx
@@ -3,20 +3,53 @@ import ListItemText from "@material-ui/core/ListItemText"
 import Activity from "components/Activity"
 
 const List = ({ type, elements }) => {
+
+  const primaryTextStyling = { style:
+    {
+      color: "white",
+      textAlign: "center",
+    },
+  }
+
+  const secondaryTextStyling = { style:
+    {
+      color: "white",
+      textAlign: "center",
+      fontSize: "1.5rem",
+    },
+  }
+
   const itemList = elements.map(elem => {
     return (
-      <ListItemLink key={elem.id}>
+      <ListItemLink key={elem.id} alignItems="center" >
         <ListItemText primary={"Stian Student Studentsen"} secondary={elem.id} />
-        <ListItemText
-          primary="Activity"
-          secondary={
-            <Activity
-              data={[0, 1, 2, 3, 2, 2, 3, 2, 3, 1, 1, 3, 3, 2, 1, 0, 0, 2, 2, 1, 0, 3, 2, 1, 0, 1, 3, 2]}
-            />}
-        />
-        <ListItemText primary="Commits" secondary={"294"} />
-        <ListItemText primary="Pull requests" secondary={"50"} />
-        <ListItemText primary="Wiki edits"secondary={"17"} />
+
+        <ListItem alignItems="flex-start">
+          <Activity
+            data={[0, 1, 2, 3, 2, 2, 3, 2, 3, 1, 1, 3, 3, 2, 1, 0, 0, 2, 2, 1, 0, 3, 2, 1, 0, 1, 3, 2]}
+          />
+        </ListItem>
+
+        <ListItem>
+          <ListItemText
+            primary="Commits"
+            secondary={"294"}
+            primaryTypographyProps={primaryTextStyling}
+            secondaryTypographyProps={secondaryTextStyling}
+          />
+          <ListItemText
+            primary="Pull requests"
+            secondary={"50"}
+            primaryTypographyProps={primaryTextStyling}
+            secondaryTypographyProps={secondaryTextStyling}
+          />
+          <ListItemText
+            primary="Wiki edits"
+            secondary={"17"}
+            primaryTypographyProps={primaryTextStyling}
+            secondaryTypographyProps={secondaryTextStyling}
+          />
+        </ListItem>
       </ListItemLink>
     )
   })
