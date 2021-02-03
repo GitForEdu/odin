347fccc0c78708f01737010bb74741e6241878c5
Tore Stensaker Tefre <tore.s.tefre@ntnu.no>
2021-02-03 11:47:36 +0100

    Mock Activity component

--- components/Activity/index.js
+++ components/Activity/index.js
@@ -1,17 +1,6 @@
-export const Activity = () => {
-  const activityData = [
-    [0, 1, 2, 3, 4, 5, 4],
-    [0, 2, 2, 0, 0, 1, 0],
-    [1, 2, 4, 5, 6, 1, 0],
-  ]
+export const Activity = props => {
+  const { data } = props
 
-  const whiteBox = (
-    <div style={{
-      width: "1em",
-      height: "1em",
-      backgroundColor: "white",
-    }} />
-  )
   return (
     <section style={{
       display: "grid",
@@ -19,34 +8,47 @@ export const Activity = () => {
       gridTemplateColumns: "repeat(7, 1em)",
       gap: "2px",
     }}>
-
-      <div style={{
-        width: "1em",
-        height: "1em",
-        backgroundColor: "lightgray",
-      }} />
-      <div style={{
-        width: "1em",
-        height: "1em",
-        backgroundColor: "darkgray",
-      }} />
-      <div style={{
-        width: "1em",
-        height: "1em",
-        backgroundColor: "white",
-      }} />
-      <div style={{
-        width: "1em",
-        height: "1em",
-        backgroundColor: "lightgray",
-      }} />
-      <div style={{
-        width: "1em",
-        height: "1em",
-        backgroundColor: "darkgray",
-      }} />
+      {data.map((digit, index) => {
+        if (digit === 0) return <WhiteBox key={index} />
+        if (digit === 1) return <LightGrayBox key={index} />
+        if (digit === 2) return <GrayBox key={index} />
+        if (digit === 3) return <DarkGrayBox key={index} />
+        return <DarkGrayBox key={index} />
+      } )}
     </section>
   )
 }
+
+const WhiteBox = () => (
+  <div style={{
+    width: "1em",
+    height: "1em",
+    backgroundColor: "white",
+  }} />
+)
+
+const LightGrayBox = () => (
+  <div style={{
+    width: "1em",
+    height: "1em",
+    backgroundColor: "lightgray",
+  }} />
+)
+
+const GrayBox = () => (
+  <div style={{
+    width: "1em",
+    height: "1em",
+    backgroundColor: "gray",
+  }} />
+)
+
+const DarkGrayBox = () => (
+  <div style={{
+    width: "1em",
+    height: "1em",
+    backgroundColor: "darkgray",
+  }} />
+)
 
 export default Activity
