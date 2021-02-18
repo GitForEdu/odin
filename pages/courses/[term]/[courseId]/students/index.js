eee34dd8931c0d41d46c2fe30a067097ac9ae253
Tore Stensaker Tefre <tore.s.tefre@ntnu.no>
2021-02-18 14:59:30 +0100

    Add mock pettegre to blackboard userlist

--- pages/courses/[term]/[courseId]/students/index.js
+++ pages/courses/[term]/[courseId]/students/index.js
@@ -25,6 +25,24 @@ export const getServerSideProps = (async (context) => {
   const params = context.params
 
   const courseUsers = await getCourseUsers(context.req, params)
+  courseUsers.push({
+    availability: { available: "Yes" },
+    avatar: { viewUrl: "https://ntnu-saastest.blackboard.com/public/v1/users/_105_1/avatar", source: "Default" },
+    courseId: "_57_1",
+    courseRoleId: "Student",
+    created: "2020-12-04T13:48:31.915Z",
+    dataSourceId: "_2_1",
+    educationLevel: "Unknown",
+    gender: "Unknown",
+    id: "_999_1",
+    institutionRoleIds: ["STUDENT"],
+    lastAccessed: "2021-02-18T12:21:53.762Z",
+    modified: "2020-12-04T13:48:31.915Z",
+    name: { given: "Petter Grø", family: "Rein" },
+    systemRoleIds: ["User"],
+    userId: "_105_1",
+    userName: "pettegre",
+  })
   // console.log("getserversideprops students", courseUsers)
 
   const groupMembers = await getGroupMembersFromGitlab(context.req, params)
