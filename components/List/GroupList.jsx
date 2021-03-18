31ede31552055b43128aba134dac4ccc266ede7a
Petter Rein <pettegre@stud.ntnu.no>
2021-03-19 00:24:37 +0100

    remove activity component

--- components/List/GroupList.jsx
+++ components/List/GroupList.jsx
@@ -50,9 +50,9 @@ const GroupList = ({ elements }) => {
               item
               xs={6}
             >
-              <Activity
+              {/* <Activity
                 data={[0, 1, 2, 3, 2, 2, 3, 2, 3, 1, 1, 3, 3, 2, 1, 0, 0, 2, 2, 1, 0, 3, 2, 1, 0, 1, 3, 2]}
-              />
+              /> */}
             </Grid>
 
 
