9b2d1341d7eb46ce4219f07c8f48eeded027911f
Petter Grø Rein <pettegre@stud.ntnu.no>
2021-04-14 16:17:32 +0200

    add datepicker fields to studentlist

--- pages/courses/[term]/[courseId]/students/index.js
+++ pages/courses/[term]/[courseId]/students/index.js
@@ -7,23 +7,193 @@ import { getAllMembersFromGitlab } from "pages/api/courses/[term]/[courseId]/git
 import { useState, Fragment } from "react"
 import { getBBGitConnection } from "pages/api/courses/[term]/[courseId]/git/createConnection"
 import groups from "pages/api/courses/[term]/[courseId]/groups"
+import { Button, FormControlLabel, Grid, Switch, TextField } from "@material-ui/core"
+import DatePicker from "@material-ui/lab/DatePicker"
 
 
 export const Students = ({ initialUsers }) => {
   const router = useRouter()
   const { courseId, term } = router.query
   const [users, setUsers] = useState(initialUsers)
+  const [sinceTime, setSinceTime] = useState(new Date("2020-01-01T00:00:00.000Z"))
+  const [untilTime, setUntilTime] = useState(new Date((new Date()).valueOf() + 86400000))
 
   return (
     <>
       <Navbar pageTitle={"All students"} courseId={courseId} term={term} />
-      <h1>Students only in GitLab (should be empty)</h1>
-      {users.gitlab && <StudentList elements={users.gitlab} />}
-      <h1>Students both in Blackboard and GitLab</h1>
-      {users.both && <StudentList elements={users.both} />}
-      <h1>Students only in Blackboard student list</h1>
-      {users.blackboard && <StudentList elements={users.blackboard} />}
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={10} md={10}>
+          <Grid
+            container
+            direction="column"
+          >
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignContent="center"
+              item
+            >
+              <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignContent="center"
+                item
+              >
+                <Grid
+                  item
+                  xs={6}
+                  md={3}
+                >
+                  <DatePicker
+                    renderInput={(props) =>
+                      <TextField
+                        {...props}
+                        margin="normal"
+                        helperText=""
+                      />}
+                    label="DatePicker"
+                    value={sinceTime}
+                    onChange={(newValue) => {
+                      setSinceTime(newValue)
+                    }}
+                  />
+                </Grid>
+                <Grid
+                  item
+                  xs={6}
+                  md={3}
+                >
+                  <DatePicker
+                    renderInput={(props) =>
+                      <TextField
+                        {...props}
+                        margin="normal"
+                        helperText=""
+                      />}
+                    label="DatePicker"
+                    value={untilTime}
+                    onChange={(newValue) => {
+                      setUntilTime(newValue)
+                    }}
+                  />
+                </Grid>
+              </Grid>
+              <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignContent="center"
+                item
+              >
+                <Grid
+                  container
+                  direction="row"
+                  justifyContent="flex-start"
+                  alignContent="center"
+                  item
+                  xs={12}
+                  md={3}
+                >
+                </Grid>
+                <Grid
+                  container
+                  direction="row"
+                  justifyContent="center"
+                  alignContent="center"
+                  item
+                  xs={12}
+                  md={6}
+                >
+                  <Grid
+                    item
+                    xs={3}
+                    md={4}
+                  >
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={() => {
+                        const date = new Date()
+                        date.setDate(date.getDate()-1)
+                        setSinceTime(date)
+                        const dateUntil = new Date()
+                        setUntilTime(dateUntil)
+                      }}
+                    >
+                Last day
+                    </Button>
+                  </Grid>
+                  <Grid
+                    item
+                    xs={3}
+                    md={4}
+                  >
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={() => {
+                        const date = new Date()
+                        date.setDate(date.getDate()-7)
+                        setSinceTime(date)
+                        const dateUntil = new Date()
+                        setUntilTime(dateUntil)
+                      }}
+                    >
+                Last week
+                    </Button>
+
+                  </Grid>
+                  <Grid
+                    item
+                    xs={3}
+                    md={4}
+                  >
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={() => {
+                        const date = new Date()
+                        date.setMonth(date.getMonth()-1)
+                        setSinceTime(date)
+                        const dateUntil = new Date()
+                        setUntilTime(dateUntil)
+                      }}
+                    >
+                Last month
+                    </Button>
+                  </Grid>
+
+                </Grid>
+                <Grid
+                  container
+                  direction="row"
+                  justifyContent="flex-start"
+                  alignContent="center"
+                  item
+                  xs={12}
+                  md={3}
+                >
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <h1>Students only in GitLab (should be empty)</h1>
+        {users.gitlab && <StudentList elements={users.gitlab} />}
+        <h1>Students both in Blackboard and GitLab</h1>
+        {users.both && <StudentList elements={users.both} />}
+        <h1>Students only in Blackboard student list</h1>
+        {users.blackboard && <StudentList elements={users.blackboard} />}
+      </Grid>
     </>
+
   )
 }
 
